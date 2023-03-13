e7224d50d086cb77311cbc2e769d42a6ccc22763
nnnnnnn1n1n <33119655+nnnnnnn1n1n@users.noreply.github.com>
2023-03-13 10:44:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ tailwind.config = {
       }
     }
   }
+
+const hRange = [0, 360];
+const sRange = [50, 75];
+const lRange = [25, 60];
+const normalizeHash = (hash, min, max) => {
+  return Math.floor((hash % (max - min)) + min);
+};
+const generateHSL = (name) => {
+  const hash = getHashOfString(name);
+  const h = normalizeHash(hash, hRange[0], hRange[1]);
+  const s = normalizeHash(hash, sRange[0], sRange[1]);
+  const l = normalizeHash(hash, lRange[0], lRange[1]);
+  return HSLtoString([h, s, l]);
+};
+
+const HSLtoString = (hsl) => {
+  return `hsl(${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%)`;
+};
+const getHashOfString = (str) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  hash = Math.abs(hash);
+  return hash;
+};
+console.log(generateHSL('Flower'));
